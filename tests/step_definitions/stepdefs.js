464aedce1b362f278393d844dc60b09e60a217de
nadvolod <nikolay.advolodkin@saucelabs.com>
2019-06-17 16:48:50 -0500

    cleanest version

--- tests/step_definitions/stepdefs.js
+++ tests/step_definitions/stepdefs.js
@@ -5,30 +5,36 @@ const hiltonHomePage = new HiltonHomePage();
 const OffersPage = require('../page/offersPage');
 const offersPage = new OffersPage();
 
-defineSupportCode(function({ Given }) {
+Given(/^I use a web browser to navigate to the Hilton home page$/, function()  {
+	hiltonHomePage.open();
+});
 
-	Given(/^I use a web browser to navigate to the Hilton home page$/, function()  {
-		hiltonHomePage.open();
-	});
+When(/^I navigate to Offers$/, function() {
+	hiltonHomePage.goToOffers();
+});
+
+Then('the Offers page loads successfully', function() {
+	assert(offersPage.isLoaded());
+});
+
+
+
+
+
+// defineSupportCode(function({ Given }) {
+
+// 	Given(/^I use a web browser to navigate to the Hilton home page$/, function()  {
+// 		hiltonHomePage.open();
+// 	});
   
-	// *** belongs to ta-loging  feature
-	Given(/^I am on the phptravels page$/, function() {
-	  loginPage.open();     // navigating to login page
-	});
+// 	// *** belongs to ta-loging  feature
+// 	Given(/^I am on the phptravels page$/, function() {
+// 	  loginPage.open();     // navigating to login page
+// 	});
   
-  });
+//   });
 
-// Given(/^I use a web browser to navigate to the Hilton home page$/, function()  {
-// 	hiltonHomePage.open();
-// });
 
-// When(/^I navigate to Offers$/, function() {
-// 	hiltonHomePage.goToOffers();
-// });
-
-// Then('the Offers page loads successfully', function() {
-// 	assert(offersPage.isLoaded());
-// });
 
 // module.exports = function() {
 // 	this.Given('^I use a web browser to navigate to the Hilton home page$', function (callback) {
